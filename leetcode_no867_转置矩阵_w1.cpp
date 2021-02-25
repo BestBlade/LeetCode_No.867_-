0d7e856572d7625c2b7e09677fd3bd44dc0d80b2
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-02-25 16:57:46 +0800

    Add files via upload

---
+++ leetcode_no867_转置矩阵_w1.cpp
@@ -0,0 +1,91 @@
+﻿/* ------------------------------------------------------------------|
+给你一个二维整数数组 matrix， 返回 matrix 的 转置矩阵 。
+
+矩阵的 转置 是指将矩阵的主对角线翻转，交换矩阵的行索引与列索引。
+
+
+
+ 
+
+示例 1：
+
+输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+输出：[[1,4,7],[2,5,8],[3,6,9]]
+示例 2：
+
+输入：matrix = [[1,2,3],[4,5,6]]
+输出：[[1,4],[2,5],[3,6]]
+ 
+
+提示：
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 1000
+1 <= m * n <= 105
+-109 <= matrix[i][j] <= 109
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/transpose-matrix
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*
+*
+*	执行用时：8 ms, 在所有 C++ 提交中击败了98.57%的用户
+*	内存消耗：10.3 MB, 在所有 C++ 提交中击败了16.44%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+    int m = matrix.size();
+    int n = matrix[0].size();
+
+    vector<vector<int>> res(n, vector<int>(m));
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            res[j][i] = matrix[i][j];
+        }
+    }
+    return res;    
+}
